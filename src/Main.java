d7a35c3bedb450d0fc528d090899fcdfb3bad704
Jean P Cruz R <jeanpcr9@gmail.com>
2025-05-29 12:32:19 -0500

    For Loop

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import java.lang.foreign.SymbolLookup;
 import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String [] args){
+    public static void main(String [] args) throws InterruptedException {
         /*
         boolean isStudent = true;
         boolean isSenior;
@@ -246,7 +247,38 @@ public class Main {
         scanner.close();
 
          */
+        //FOR LOOP
+        /*
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter how many times you want to loop: ");
+        int max = scanner.nextInt();
+
+        for (int i = 1; i <= max; i++){
+
+            System.out.println(i);
+        }
+
+        scanner.close();
+
+         */
+        //COUNTDOWN
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many seconds to countdown from?: ");
+        int start = scanner.nextInt();
+
+        for (int i = start; i > 0; i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+
+        System.out.println("HAPPY NEW YEAR!");
+
+        scanner.close();
 
 
+        }
 
     }
